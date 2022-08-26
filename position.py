cb8493b4915548313cbc4a4aea09cd39daf9b8c5
Mohamed Fares Ben ayed <faresaction@yahoo.fr>
2022-08-26 09:42:04 +0200

    add encryption and decryption feature in position.py

--- position.py
+++ position.py
@@ -4,10 +4,27 @@ def have(char, k):
     res = chr(nxt + start)
     return res
 
+def be(char, k):
+    start = 65
+    idx = (ord(char) - start)
+    if k > idx:
+        res = 26 - k + idx
+        res = chr(res + start)
+    else:
+        res = idx - k
+        res = chr(res + start) 
+    return res
+
 def encrypt(word):
     ans = ""
     for idx, ltr in enumerate(word):
         ans += have(ltr, idx + 1)
+    return ans
+
+def decrypt(word):
+    ans = ""
+    for idx, ltr in enumerate(word):
+        ans += be(ltr, idx + 1)
     return ans
 
 def CompleteEncryption(msg):
@@ -18,8 +35,19 @@ def CompleteEncryption(msg):
         msgEnc.append(wrdEnc)
     return ' '.join(msgEnc)
 
+def CompleteDecryption(msg):
+    msg = msg.upper().split()
+    msgEnc = []
+    for wrd in msg:
+        wrdEnc = decrypt(wrd)
+        msgEnc.append(wrdEnc)
+    return ' '.join(msgEnc)
+
 msg = "The way is not easy for me to pass But I will try and try again"
-print(CompleteEncryption(msg))
+EncMsg = CompleteEncryption(msg)
+print(EncMsg)
+DecMsg = CompleteDecryption(EncMsg)
+print(DecMsg)
 
 
 
